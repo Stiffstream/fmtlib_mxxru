c81ae76f55341d5041c9e9636fd387b6c8bc4b7a
hg@nshmakov <hg@nshmakov>
2016-04-07 18:58:13 +0300

    Old code removed.

--- dev/cppformat/prj.rb
+++ dev/cppformat/prj.rb
@@ -6,10 +6,6 @@ MxxRu::Cpp::composite_target {
 
   include_path( 'cppformat', MxxRu::Cpp::Target::OPT_UPSPREAD )
 
-  # if 'gcc' == toolset.name || 'clang' == toolset.name
-  #   global_linker_option( "-pthread" )
-  # end
-
   define( 'FMT_HEADER_ONLY', Mxx_ru::Cpp::Target::OPT_UPSPREAD )
 }
 
