22c5343536c3b81fa3aaa3bb1afc7050af92512d
ngrodzitski <utromvecherom@gmail.com>
2018-06-12 12:37:27 +0300

    Upgrade fmtlib to 4.1.0.

--- externals.rb
+++ externals.rb
@@ -1,6 +1,5 @@
 MxxRu::arch_externals :fmt do |e|
-  e.url 'https://github.com/fmtlib/fmt/archive/3.0.0.zip'
+  e.url 'https://github.com/fmtlib/fmt/archive/4.1.0.zip'
 
-  e.map_dir 'cppformat' => 'dev/fmt'
   e.map_dir 'fmt' => 'dev/fmt'
 end
